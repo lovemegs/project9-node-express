64c40ab90926aa47ecc73ee7ebe2ead6d3bbf4ae
Megan Love <lovemegs94@hotmail.com>
2022-10-09 17:50:30 -0600

    c

--- app.js
+++ app.js
@@ -9,30 +9,30 @@ app.use('/static', express.static('public'));
 
 // ROUTES
 app.get('/', (req, res) => {
-	const projects = res.locals = data.projects;
+	res.locals.projects = data.projects;
 	res.render('index');
 });
 app.get('/about', (req, res) => {
 	res.render('about')
 });
 app.get('/project/:id', (req, res) => {
-	const projects = res.locals = data.projects;
+	res.locals.projects = data.projects;
 	res.render('project');
 });
 
 
 // ERROR 404
-app.use((req, res, next) => {
-	const error = new Error('Page Not Found');
-	err.status = 404;
-	next(error);
-});
-app.use((err, req, res, next) =>{
-	console.log(`${err.status} ${err.message}`);
-	res.locals.error = err;
-	res.status(err.status);
-	res.render('error');
-});
+// app.use((req, res, next) => {
+// 	const error = new Error('Page Not Found');
+// 	err.status = 404;
+// 	next(error);
+// });
+// app.use((err, req, res, next) =>{
+// 	console.log(`${err.status} ${err.message}`);
+// 	res.locals.error = err;
+// 	res.status(err.status);
+// 	res.render('error');
+// });
 
 // LOCALHOST 3000
 app.listen(3000, () => {
